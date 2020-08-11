f69006a2930a40cc4df666bc7c4dad5d196240c8
Anurag Kulkarni <69456413+Avk14@users.noreply.github.com>
2020-08-11 22:22:54 +0530

    Add files via upload

---
+++ exercise 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace  std;
+int main()
+{
+	int x,y;
+	cout<<"enter no.1"<<endl;
+	cin>>x;
+	cout<<"\nenter no.2"<<endl;
+	cin>>y;
+	cout<<"\nproduct is="<<x*y;
+	
+}
