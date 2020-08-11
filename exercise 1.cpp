5cedcbd81fbc5325e5f5061c1855e66dd5375d9c
Anurag Kulkarni <69456413+Avk14@users.noreply.github.com>
2020-08-11 22:09:07 +0530

    Add files via upload

---
+++ exercise 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace  std;
+int main()
+{
+	int a;
+	float b;
+	double c;
+	char d;
+	cout<<"size of int="<<sizeof(a)<<"\n";
+	cout<<"size of float="<<sizeof(b)<<"\n";
+	cout<<"size of double="<<sizeof(c)<<endl;
+	cout<<"size of char="<<sizeof(d)<<"\n";
+	_getch();
+}
+
